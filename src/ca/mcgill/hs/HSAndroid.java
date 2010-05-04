8b7a4d988f445a703ee7c577c8e556b01efb7f35
Cicerone Cojocaru <cicerone.cojocaru@mail.mcgill.ca>
2010-05-04 11:03:42 -0400

    changed message

--- src/ca/mcgill/hs/HSAndroid.java
+++ src/ca/mcgill/hs/HSAndroid.java
@@ -5,6 +5,7 @@ package ca.mcgill.hs;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class HSAndroid extends Activity {
     /** Called when the activity is first created. */
@@ -12,5 +13,8 @@ public class HSAndroid extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        TextView tv = new TextView(this);
+        tv.setText("Hello android!");
+        setContentView(tv);
     }
 }

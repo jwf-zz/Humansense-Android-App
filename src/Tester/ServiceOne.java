7a7e01b843ba0a7199d57df84c6d69ad6dd0f72a
Jonathan Pitre <jonathan.pitre@mail.mcgill.ca>
2010-05-04 10:14:36 -0400

    Change one.

---
+++ src/Tester/ServiceOne.java
@@ -0,0 +1,16 @@
+package Tester;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+
+public class ServiceOne extends Service {
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

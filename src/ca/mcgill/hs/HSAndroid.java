4e5548f3fbab2f66b9c82e90b283f080d913e458
Jonathan Pitre <jonathan.pitre@mail.mcgill.ca>
2010-05-05 10:00:10 -0400

    Made basic framework for Activity-Service interfacing.

--- src/ca/mcgill/hs/HSAndroid.java
+++ src/ca/mcgill/hs/HSAndroid.java
@@ -10,18 +10,37 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.view.View.OnClickListener;
 
-public class HSAndroid extends Activity {
+
+public class HSAndroid extends Activity implements OnClickListener{
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        TextView tv = new TextView(this);
-        tv.setText("Hello android!");
-        setContentView(tv);
-        //This is in the branch. 
+        
+        //Buttons
+        View startButton = findViewById(R.id.startButton);
+        startButton.setOnClickListener(this);
+        View stopButton = findViewById(R.id.stopButton);
+        stopButton.setOnClickListener(this);
+
     }
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()){
+		
+		case R.id.startButton: //START BUTTON CASE
+			break;
+			
+		case R.id.stopButton: //STOP BUTTON CASE
+			break;
+			
+		}
+	}
     
 }

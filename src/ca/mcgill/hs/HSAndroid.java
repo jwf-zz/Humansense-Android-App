453df8e29dc87b1b518ff8a4cee32e189ff19ab1
Jonathan Pitre <jonathan.pitre@mail.mcgill.ca>
2010-05-06 12:43:55 -0400

    Cleaned code.

--- src/ca/mcgill/hs/HSAndroid.java
+++ src/ca/mcgill/hs/HSAndroid.java
@@ -10,7 +10,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;

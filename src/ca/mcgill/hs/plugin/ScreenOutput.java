c2b0e067229278fd7e50652cdb43f276e5debbc9
Jonathan Pitre <jonathan.pitre@mail.mcgill.ca>
2010-06-21 09:26:22 -0400

    Screen output plugin updated with GSM data.

--- src/ca/mcgill/hs/plugin/ScreenOutput.java
+++ src/ca/mcgill/hs/plugin/ScreenOutput.java
@@ -3,6 +3,7 @@ package ca.mcgill.hs.plugin;
 import ca.mcgill.hs.plugin.SensorLogger.SensorLoggerPacket;
 import ca.mcgill.hs.plugin.WifiLogger.WifiLoggerPacket;
 import ca.mcgill.hs.plugin.GPSLogger.GPSLoggerPacket;
+import ca.mcgill.hs.plugin.GSMLogger.GSMLoggerPacket;
 import android.util.Log;
 
 
@@ -28,8 +29,8 @@ public class ScreenOutput extends OutputPlugin{
 			dataParse((WifiLoggerPacket) dp);
 		} else if (dp.getClass() == GPSLoggerPacket.class) {
 			dataParse((GPSLoggerPacket) dp);
-		} else if (dp.getClass() == SensorLoggerPacket.class){
-			
+		} else if (dp.getClass() == GSMLoggerPacket.class){
+			dataParse((GSMLoggerPacket) dp);
 		}
 	}
 	
@@ -38,14 +39,14 @@ public class ScreenOutput extends OutputPlugin{
 	 * @param wlp the WifiLoggerPacket to parse and write out.
 	 */
 	private void dataParse(WifiLoggerPacket wlp){
-		Log.i("ScreenOutput", "Time: " + wlp.timestamp);
-		Log.i("ScreenOutput", "Neighbors: " + wlp.neighbors);
+		Log.i("WifiLogger SO", "Time: " + wlp.timestamp);
+		Log.i("WifiLogger SO", "Neighbors: " + wlp.neighbors);
 		int j = wlp.levels.length;
 		for (int i = 0; i<j ; i++){
 			Log.i("WifiLogger SO", "SSID: " + wlp.SSIDs[i]);
-			Log.i("ScreenOutput", "Level: " + wlp.levels[i]);
-			Log.i("ScreenOutput", "BSSID: " + wlp.BSSIDs[i]);
-			Log.i("ScreenOutput", " ");
+			Log.i("WifiLogger SO", "Level: " + wlp.levels[i]);
+			Log.i("WifiLogger SO", "BSSID: " + wlp.BSSIDs[i]);
+			Log.i("WifiLogger SO", " ");
 		}
 	}
 	
@@ -55,6 +56,21 @@ public class ScreenOutput extends OutputPlugin{
 	 */
 	private void dataParse(GPSLoggerPacket gpslp){
 		Log.i("GPSLocationLogger SO", "Area: [" + gpslp.altitude + "][" + gpslp.latitude + "][" + gpslp.longitude + "]");
+	}
+	
+	private void dataParse(GSMLoggerPacket gsmlp){
+		Log.i("GSMLogger SO", "Timestamp : " + gsmlp.time);
+		Log.i("GSMLogger SO", "MCC : " + gsmlp.mcc);
+		Log.i("GSMLogger SO", "MNC : " + gsmlp.mnc);
+		Log.i("GSMLogger SO", "CID : " + gsmlp.cid);
+		Log.i("GSMLogger SO", "LAC : " + gsmlp.lac);
+		Log.i("GSMLogger SO", "RSSI : " + gsmlp.rssi);
+		Log.i("GSMLogger SO", "Neighbors : " + gsmlp.neighbors);
+		for (int i = gsmlp.neighbors - 1; i >= 0; i--){
+			Log.i("GSMLogger SO", "Neighbor " + i + " CID : " + gsmlp.cids[i]);
+			Log.i("GSMLogger SO", "Neighbor " + i + " LAC : " + gsmlp.lacs[i]);
+			Log.i("GSMLogger SO", "Neighbor " + i + " RSSI : " + gsmlp.rssis[i]);
+		}
 	}
 
 }

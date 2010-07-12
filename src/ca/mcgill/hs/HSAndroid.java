b885cbd73a16a88e97be5aeac71a5cf0659d575d
Cicerone Cojocaru <cicerone.cojocaru@mail.mcgill.ca>
2010-07-12 09:06:23 -0400

    picking up pieces after auto-formatting :(

--- src/ca/mcgill/hs/HSAndroid.java
+++ src/ca/mcgill/hs/HSAndroid.java
@@ -5,8 +5,6 @@ package ca.mcgill.hs;
 
 import java.net.HttpURLConnection;
 
-import ca.mcgill.hs.serv.HSService;
-
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
@@ -19,178 +17,192 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+import ca.mcgill.hs.serv.HSService;
 
 /**
- * This Activity is the entry point to the HSAndroid application. This Activity is launched
- * manually on the phone by the user, and is from where the background services can be manually
- * started and stopped, and where the preferences and settigns can be changed.
+ * This Activity is the entry point to the HSAndroid application. This Activity
+ * is launched manually on the phone by the user, and is from where the
+ * background services can be manually started and stopped, and where the
+ * preferences and settigns can be changed.
  * 
  * @author Jonathan Pitre
  * 
  */
-public class HSAndroid extends Activity{
-	
+public class HSAndroid extends Activity {
+
+	// **************************************************************************
+	// INNER CLASS - FILE UPLOADER
+	// **************************************************************************
+	// TODO: Update strings.xml with new static strings.
+	private class FileUploader {
+
+		private final String URL_STRING = "http://www.cs.mcgill.ca/~ccojoc2/uploader.php";
+		private final String DIR_PATH = (String) getResources().getText(
+				R.string.data_file_path);
+		private HttpURLConnection conn;
+
+		/**
+		 * Gets called whenever the file upload is complete.
+		 */
+		private void onUploadComplete() {
+			uploading = false;
+			uploadButton.setEnabled(true);
+			uploadButton.setText("UPLOAD");
+		}
+
+		/**
+		 * Uploads most recent files to the server defined in URL_STRING.
+		 */
+		private void upload() {
+			uploading = true;
+			uploadButton.setEnabled(false);
+			uploadButton.setText("Uploading...");
+
+			// The thread in which the files will be uploaded.
+			new Thread() {
+				@Override
+				public void run() {
+
+					onUploadComplete();
+				}
+			}.start();
+		}
+	}
+
 	private static Button button;
 	private static Button uploadButton;
+
 	private Intent i;
-	
+
 	private boolean autoStartAppStart = false;
-	
 	public static final String HSANDROID_PREFS_NAME = "HSAndroidPrefs";
+
 	private static final int MENU_SETTINGS = 37043704;
-	
 	private FileUploader fu;
+
 	private static boolean uploading = false;
-	
-    /**
-     * This method is called when the activity is first created. It is the entry
-     * point for the application.
-     * 
-     * @override
-     */
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        
-        //Creating the fu
-        fu = new FileUploader();
-        
-        //Intent
-        i = new Intent(this, HSService.class);
-        
-        //Setup preferences
-        getPrefs();
-        
-        //Auto App Start
-        if (autoStartAppStart){
-        	startService(i);
-        }
-        
-        //Buttons
-        button = (Button) findViewById(R.id.button);
-        button.setText(HSService.isRunning() ? R.string.stop_label : R.string.start_label);
-        button.setOnClickListener( new View.OnClickListener() {
-			public void onClick(View v) {
-				if (!HSService.isRunning()){ //NOT RUNNING
+
+	/**
+	 * Updates the main starting button. This is required due to the nature of
+	 * Activities in the Android API. In order to correctly get the state of the
+	 * service to update the button text, this method cannot be called from
+	 * within the Activity.
+	 */
+	public static void updateButton() {
+		if (button != null) {
+			button.setText((HSService.isRunning() ? R.string.stop_label
+					: R.string.start_label));
+		}
+	}
+
+	/**
+	 * Sets up the preferences, i.e. get Activity preferences.
+	 */
+	private void getPrefs() {
+		final SharedPreferences prefs = PreferenceManager
+				.getDefaultSharedPreferences(getBaseContext());
+		autoStartAppStart = prefs.getBoolean("autoStartAtAppStart", false);
+	}
+
+	/**
+	 * This method is called when the activity is first created. It is the entry
+	 * point for the application.
+	 */
+	@Override
+	public void onCreate(final Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		// Creating the fu
+		fu = new FileUploader();
+
+		// Intent
+		i = new Intent(this, HSService.class);
+
+		// Setup preferences
+		getPrefs();
+
+		// Auto App Start
+		if (autoStartAppStart) {
+			startService(i);
+		}
+
+		// Buttons
+		button = (Button) findViewById(R.id.button);
+		button.setText(HSService.isRunning() ? R.string.stop_label
+				: R.string.start_label);
+		button.setOnClickListener(new View.OnClickListener() {
+			public void onClick(final View v) {
+				if (!HSService.isRunning()) { // NOT RUNNING
 					startService(i);
 					button.setText(R.string.stop_label);
-				} else { //RUNNING
+				} else { // RUNNING
 					stopService(i);
 					button.setText(R.string.start_label);
 				}
 			}
 		});
-        
-        //YES-NO DIALOG BOX FOR FILE UPLOAD
-        final Context c = this;
-        DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
-		    @Override
-		    public void onClick(DialogInterface dialog, int which) {
-		        switch (which){
-		        case DialogInterface.BUTTON_POSITIVE:
-		            fu.upload();
-		            break;
 
-		        case DialogInterface.BUTTON_NEGATIVE:
-		            break;
-		        }
-		    }
+		// YES-NO DIALOG BOX FOR FILE UPLOAD
+		final Context c = this;
+		final DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(final DialogInterface dialog, final int which) {
+				switch (which) {
+				case DialogInterface.BUTTON_POSITIVE:
+					fu.upload();
+					break;
+
+				case DialogInterface.BUTTON_NEGATIVE:
+					break;
+				}
+			}
 		};
 
 		final AlertDialog.Builder builder = new AlertDialog.Builder(this);
 		builder.setMessage("Upload data collected to online server?")
-			.setPositiveButton("Yes", dialogClickListener).setNegativeButton("No", dialogClickListener);
-		
-        uploadButton = (Button) findViewById(R.id.uploadButton);
-        uploadButton.setOnClickListener( new View.OnClickListener() {
-			public void onClick(View v) {
+				.setPositiveButton("Yes", dialogClickListener)
+				.setNegativeButton("No", dialogClickListener);
+
+		uploadButton = (Button) findViewById(R.id.uploadButton);
+		uploadButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(final View v) {
 				builder.show();
 			}
 		});
-        
-        if (uploading){ 
-        	uploadButton.setEnabled(false);
-        	uploadButton.setText("Uploading...");
-        }
-        
-    }
-    
-    /**
-     * This method is called whenever the user wants to access the settings menu.
-     */
-    public boolean onCreateOptionsMenu(Menu menu) {
-        menu.add(0, MENU_SETTINGS, 0, R.string.settingString).setIcon(R.drawable.options);
-        return true;
-    }
 
-    /**
-     * This method is used to parse the selection of options items. These items include:
-     * - Preferences (settings)
-     */
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-        case MENU_SETTINGS:
-            Intent i = new Intent(getBaseContext(), ca.mcgill.hs.prefs.HSAndroidPreferences.class);
-            startActivity(i);
-            break;
-        }
-        return false;
-    }
-    
-    /**
-     * Sets up the preferences, i.e. get Activity preferences.
-     */
-    private void getPrefs(){
-    	SharedPreferences prefs = 
-    		PreferenceManager.getDefaultSharedPreferences(getBaseContext());
-    	autoStartAppStart = prefs.getBoolean("autoStartAtAppStart", false);
-    }
-    
-    /**
-     * Updates the main starting button. This is required due to the nature of Activities
-     * in the Android API. In order to correctly get the state of the service to update
-     * the button text, this method cannot be called from within the Activity.
-     */
-    public static void updateButton(){
-    	if (button != null) button.setText((HSService.isRunning() ? R.string.stop_label : R.string.start_label));
-    }
-    
-    //**************************************************************************
-    // INNER CLASS - FILE UPLOADER
-    //**************************************************************************
-    //TODO: Update strings.xml with new static strings.
-    private class FileUploader{
-    	
-    	private final String URL_STRING = "http://www.cs.mcgill.ca/~ccojoc2/uploader.php";
-    	private final String DIR_PATH = (String)getResources().getText(R.string.data_file_path);
-    	private HttpURLConnection conn;
-    	
-    	/**
-    	 * Uploads most recent files to the server defined in URL_STRING.
-    	 */
-    	private void upload(){
-    		uploading = true;
-    		uploadButton.setEnabled(false);
-    		uploadButton.setText("Uploading...");
-    		
-    		//The thread in which the files will be uploaded.
-    		new Thread(){
-    			public void run(){
-    				
-    				onUploadComplete();
-    			}
-    		}.start();
-    	}
-    	
-    	/**
-    	 * Gets called whenever the file upload is complete.
-    	 */
-    	private void onUploadComplete(){
-    		uploading = false;
-    		uploadButton.setEnabled(true);
-    		uploadButton.setText("UPLOAD");
-    	}
-    }
-    
+		if (uploading) {
+			uploadButton.setEnabled(false);
+			uploadButton.setText("Uploading...");
+		}
+
+	}
+
+	/**
+	 * This method is called whenever the user wants to access the settings
+	 * menu.
+	 */
+	@Override
+	public boolean onCreateOptionsMenu(final Menu menu) {
+		menu.add(0, MENU_SETTINGS, 0, R.string.settingString).setIcon(
+				R.drawable.options);
+		return true;
+	}
+
+	/**
+	 * This method is used to parse the selection of options items. These items
+	 * include: - Preferences (settings)
+	 */
+	@Override
+	public boolean onOptionsItemSelected(final MenuItem item) {
+		switch (item.getItemId()) {
+		case MENU_SETTINGS:
+			final Intent i = new Intent(getBaseContext(),
+					ca.mcgill.hs.prefs.HSAndroidPreferences.class);
+			startActivity(i);
+			break;
+		}
+		return false;
+	}
+
 }
--- src/ca/mcgill/hs/plugin/GPSLogger.java
+++ src/ca/mcgill/hs/plugin/GPSLogger.java
@@ -1,8 +1,5 @@
 package ca.mcgill.hs.plugin;
 
-import ca.mcgill.hs.R;
-import ca.mcgill.hs.util.PreferenceFactory;
-
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.location.Location;
@@ -13,205 +10,59 @@ import android.os.Looper;
 import android.preference.Preference;
 import android.preference.PreferenceManager;
 import android.util.Log;
+import ca.mcgill.hs.R;
+import ca.mcgill.hs.util.PreferenceFactory;
 
 /**
  * An InputPlugin which gets data from the available GPS signals around.
  * 
  * @author Cicerone Cojocaru, Jonathan Pitre
- *
+ * 
  */
-public class GPSLogger extends InputPlugin{
-	
-	//Boolean ON-OFF switch *Temporary only*
-	private boolean PLUGIN_ACTIVE;
-	
-	//The LocationManager used to request location updates.
-	private final LocationManager gpsm;
-	
-	//A GPSLocationListener which listens for location updates.
-	private final GPSLocationListener gpsll;
-	
-	//The minimum distance the person has to travel for an update to be valid.
-	private int MIN_DIST;
-	
-	//The minimum amount of time, in milliseconds, that has to pass between two subsequent updates
-	//for an update to be valid.
-	private int UPDATE_FREQ;
-	
-	//The Context for the preferences.
-	private final Context context;
-	
-	/**
-	 * This is the basic constructor for the GPSLogger plugin. It has to be instantiated
-	 * before it is started, and needs to be passed a reference to a LocationManager and a Context.
-	 * 
-	 * @param gpsm the LocationManager used for this plugin.
-	 * @param context needed for the Preference objects.
-	 */
-	public GPSLogger(LocationManager gpsm, Context context){
+public class GPSLogger extends InputPlugin {
 
-		this.gpsm = gpsm;
-		gpsll = new GPSLocationListener(gpsm);
-		
-		this.context = context;
-		
-		SharedPreferences prefs = 
-    		PreferenceManager.getDefaultSharedPreferences(context);		
-		PLUGIN_ACTIVE = prefs.getBoolean("gpsLoggerEnable", false);
-		MIN_DIST = Integer.parseInt(prefs.getString("gpsLoggerDistancePreference", "0"));
-		UPDATE_FREQ = Integer.parseInt(prefs.getString("gpsLoggerIntervalPreference", "30000"));
-	}
-	
-	/**
-	 * This method requests location updates from the LocationManager.
-	 *
-	 * @Override
-	 */
-	public void startPlugin() {
-		if (!PLUGIN_ACTIVE) return;
-		gpsm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 
-				UPDATE_FREQ,
-				MIN_DIST, 
-				gpsll, Looper.getMainLooper());
-		Log.i("GPSLocationLogger local", "Registered Location Listener.");
-	}
-
-
-	/**
-	 * This method removes the requested location updates.
-	 * 
-	 * @Override
-	 */
-	public void stopPlugin() {
-		if (!PLUGIN_ACTIVE) return;
-		gpsm.removeUpdates(gpsll);
-		Log.i("GPSLocationLogger local", "Unregistered Location Listener.");
-	}
-	
-	/**
-	 * Creates a GPSLocationPacket with the current location's coordinates.
-	 * 
-	 * @param loc the current Location.
-	 */
-	private void getNewLocation(Location loc){
-		Log.i("GPSLocationLogger local", "Data received.");
-		write(new GPSLoggerPacket(
-				loc.getTime(), loc.getAccuracy(), loc.getBearing(), loc.getSpeed(),
-				loc.getAltitude(), loc.getLatitude(), loc.getLongitude()));
-	}
-	
-	/**
-	 * Returns the list of Preference objects for this InputPlugin.
-	 * 
-	 * @param c the context for the generated Preferences.
-	 * @return an array of the Preferences of this object.
-	 * 
-	 * @override
-	 */
-	public static Preference[] getPreferences(Context c){
-		Preference[] prefs = new Preference[3];
-		
-		prefs[0] = PreferenceFactory.getCheckBoxPreference(c, "gpsLoggerEnable",
-				"GPS Plugin", "Enables or disables this plugin.",
-				"GPSLogger is on.", "GPSLogger is off.");
-		
-		prefs[1] = PreferenceFactory.getListPreference(c, R.array.gpsLoggerIntervalStrings,
-				R.array.gpsLoggerIntervalValues, "30000", "gpsLoggerIntervalPreference",
-				R.string.gpslogger_interval_pref, R.string.gpslogger_interval_pref_summary);
-		
-		prefs [2] = PreferenceFactory.getListPreference(c, R.array.gpsLoggerDistanceStrings,
-				R.array.gpsLoggerDistanceValues, "0", "gpsLoggerDistancePreference",
-				R.string.gpslogger_distance_pref, R.string.gpslogger_distance_pref_summary);
-		
-		return prefs;
-	}
-	
-	/**
-	 * Returns whether or not this InputPlugin has Preferences.
-	 * 
-	 * @return whether or not this InputPlugin has preferences.
-	 */
-	public static boolean hasPreferences(){return true;}
-	
-	/**
-	 * This method gets called whenever the preferences have been changed.
-	 * 
-	 * @Override
-	 */
-	public void onPreferenceChanged(){
-		SharedPreferences prefs = 
-    		PreferenceManager.getDefaultSharedPreferences(context);
-		
-		boolean new_PLUGIN_ACTIVE = prefs.getBoolean("gpsLoggerEnable", false);
-		if (PLUGIN_ACTIVE && !new_PLUGIN_ACTIVE){
-			stopPlugin();
-			PLUGIN_ACTIVE = new_PLUGIN_ACTIVE;
-		} else if (!PLUGIN_ACTIVE && new_PLUGIN_ACTIVE){
-			PLUGIN_ACTIVE = new_PLUGIN_ACTIVE;
-			startPlugin();
-		}
-		
-		if (PLUGIN_ACTIVE){
-			gpsm.removeUpdates(gpsll);
-			MIN_DIST = Integer.parseInt(prefs.getString("gpsLoggerDistancePreference", "0"));
-			UPDATE_FREQ = Integer.parseInt(prefs.getString("gpsLoggerIntervalPreference", "30000"));
-			gpsm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 
-					UPDATE_FREQ,
-					MIN_DIST, 
-					gpsll, Looper.getMainLooper());
-		}
-	}
-	
-	
-	// ***********************************************************************************
-	// PRIVATE INNER CLASS -- GPSLocationListener
-	// ***********************************************************************************
-	
-	private class GPSLocationListener implements android.location.LocationListener {
+	private class GPSLocationListener implements
+			android.location.LocationListener {
 
 		private static final String TAG = "GPSLocationLogger";
-		
+
 		@SuppressWarnings("unused")
 		private final LocationManager lm;
-		
+
 		@SuppressWarnings("unused")
 		private boolean available = false;
 
-		public GPSLocationListener(LocationManager lm) {
+		public GPSLocationListener(final LocationManager lm) {
 			this.lm = lm;
 		}
-		
-		public void onLocationChanged(Location location) {
+
+		public void onLocationChanged(final Location location) {
 			getNewLocation(location);
 		}
 
-		public void onProviderDisabled(String provider) {
+		public void onProviderDisabled(final String provider) {
 			Log.d(TAG, "GPS onProviderEnabled called.");
 			available = false;
 		}
 
-		public void onProviderEnabled(String provider) {
+		public void onProviderEnabled(final String provider) {
 			Log.d(TAG, "GPS onProviderEnabled called.");
 		}
 
-		public void onStatusChanged(String provider, int status, Bundle extras) {
+		public void onStatusChanged(final String provider, final int status,
+				final Bundle extras) {
 			if (status == LocationProvider.AVAILABLE) {
-				Log.d(TAG,"GPS Is Available.");
+				Log.d(TAG, "GPS Is Available.");
 				available = true;
-			}
-			else {
-				Log.d(TAG,"GPS Is Not Available.");
+			} else {
+				Log.d(TAG, "GPS Is Not Available.");
 				available = false;
 			}
 		}
 	}
-	
-	// ***********************************************************************************
-	// PUBLIC INNER CLASS -- GPSLocationPacket
-	// ***********************************************************************************
-	
-	public static class GPSLoggerPacket implements DataPacket{
-		
+
+	public static class GPSLoggerPacket implements DataPacket {
+
 		final long time;
 		final float accuracy;
 		final float bearing;
@@ -221,8 +72,10 @@ public class GPSLogger extends InputPlugin{
 		final double longitude;
 		final static String PLUGIN_NAME = "GPSLogger";
 		final static int PLUGIN_ID = PLUGIN_NAME.hashCode();
-		
-		public GPSLoggerPacket(long time, float accuracy, float bearing, float speed, double altitude, double latitude, double longitude){
+
+		public GPSLoggerPacket(final long time, final float accuracy,
+				final float bearing, final float speed, final double altitude,
+				final double latitude, final double longitude) {
 			this.time = time;
 			this.accuracy = accuracy;
 			this.bearing = bearing;
@@ -233,8 +86,9 @@ public class GPSLogger extends InputPlugin{
 		}
 
 		@Override
-		public DataPacket clone(){
-			return new GPSLoggerPacket(time, accuracy, bearing, speed, altitude, latitude, longitude);
+		public DataPacket clone() {
+			return new GPSLoggerPacket(time, accuracy, bearing, speed,
+					altitude, latitude, longitude);
 		}
 
 		@Override
@@ -247,6 +101,162 @@ public class GPSLogger extends InputPlugin{
 			return GPSLoggerPacket.PLUGIN_NAME;
 		}
 
+	}
+
+	/**
+	 * Returns the list of Preference objects for this InputPlugin.
+	 * 
+	 * @param c
+	 *            the context for the generated Preferences.
+	 * @return an array of the Preferences of this object.
+	 */
+	public static Preference[] getPreferences(final Context c) {
+		final Preference[] prefs = new Preference[3];
+
+		prefs[0] = PreferenceFactory.getCheckBoxPreference(c,
+				"gpsLoggerEnable", "GPS Plugin",
+				"Enables or disables this plugin.", "GPSLogger is on.",
+				"GPSLogger is off.");
+
+		prefs[1] = PreferenceFactory.getListPreference(c,
+				R.array.gpsLoggerIntervalStrings,
+				R.array.gpsLoggerIntervalValues, "30000",
+				"gpsLoggerIntervalPreference",
+				R.string.gpslogger_interval_pref,
+				R.string.gpslogger_interval_pref_summary);
+
+		prefs[2] = PreferenceFactory.getListPreference(c,
+				R.array.gpsLoggerDistanceStrings,
+				R.array.gpsLoggerDistanceValues, "0",
+				"gpsLoggerDistancePreference",
+				R.string.gpslogger_distance_pref,
+				R.string.gpslogger_distance_pref_summary);
+
+		return prefs;
+	}
+
+	/**
+	 * Returns whether or not this InputPlugin has Preferences.
+	 * 
+	 * @return whether or not this InputPlugin has preferences.
+	 */
+	public static boolean hasPreferences() {
+		return true;
+	}
+
+	// Boolean ON-OFF switch *Temporary only*
+	private boolean PLUGIN_ACTIVE;
+
+	// The LocationManager used to request location updates.
+	private final LocationManager gpsm;
+
+	// A GPSLocationListener which listens for location updates.
+	private final GPSLocationListener gpsll;
+
+	// The minimum distance the person has to travel for an update to be valid.
+	private int MIN_DIST;
+
+	// The minimum amount of time, in milliseconds, that has to pass between two
+	// subsequent updates
+	// for an update to be valid.
+	private int UPDATE_FREQ;
+
+	// The Context for the preferences.
+	private final Context context;
+
+	/**
+	 * This is the basic constructor for the GPSLogger plugin. It has to be
+	 * instantiated before it is started, and needs to be passed a reference to
+	 * a LocationManager and a Context.
+	 * 
+	 * @param gpsm
+	 *            the LocationManager used for this plugin.
+	 * @param context
+	 *            needed for the Preference objects.
+	 */
+	public GPSLogger(final LocationManager gpsm, final Context context) {
+
+		this.gpsm = gpsm;
+		gpsll = new GPSLocationListener(gpsm);
+
+		this.context = context;
+
+		final SharedPreferences prefs = PreferenceManager
+				.getDefaultSharedPreferences(context);
+		PLUGIN_ACTIVE = prefs.getBoolean("gpsLoggerEnable", false);
+		MIN_DIST = Integer.parseInt(prefs.getString(
+				"gpsLoggerDistancePreference", "0"));
+		UPDATE_FREQ = Integer.parseInt(prefs.getString(
+				"gpsLoggerIntervalPreference", "30000"));
+	}
+
+	/**
+	 * Creates a GPSLocationPacket with the current location's coordinates.
+	 * 
+	 * @param loc
+	 *            the current Location.
+	 */
+	private void getNewLocation(final Location loc) {
+		Log.i("GPSLocationLogger local", "Data received.");
+		write(new GPSLoggerPacket(loc.getTime(), loc.getAccuracy(), loc
+				.getBearing(), loc.getSpeed(), loc.getAltitude(), loc
+				.getLatitude(), loc.getLongitude()));
+	}
+
+	/**
+	 * This method gets called whenever the preferences have been changed.
+	 */
+	@Override
+	public void onPreferenceChanged() {
+		final SharedPreferences prefs = PreferenceManager
+				.getDefaultSharedPreferences(context);
+
+		final boolean new_PLUGIN_ACTIVE = prefs.getBoolean("gpsLoggerEnable",
+				false);
+		if (PLUGIN_ACTIVE && !new_PLUGIN_ACTIVE) {
+			stopPlugin();
+			PLUGIN_ACTIVE = new_PLUGIN_ACTIVE;
+		} else if (!PLUGIN_ACTIVE && new_PLUGIN_ACTIVE) {
+			PLUGIN_ACTIVE = new_PLUGIN_ACTIVE;
+			startPlugin();
+		}
+
+		if (PLUGIN_ACTIVE) {
+			gpsm.removeUpdates(gpsll);
+			MIN_DIST = Integer.parseInt(prefs.getString(
+					"gpsLoggerDistancePreference", "0"));
+			UPDATE_FREQ = Integer.parseInt(prefs.getString(
+					"gpsLoggerIntervalPreference", "30000"));
+			gpsm.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+					UPDATE_FREQ, MIN_DIST, gpsll, Looper.getMainLooper());
+		}
+	}
+
+	/**
+	 * This method requests location updates from the LocationManager.
+	 * 
+	 * @Override
+	 */
+	public void startPlugin() {
+		if (!PLUGIN_ACTIVE) {
+			return;
+		}
+		gpsm.requestLocationUpdates(LocationManager.GPS_PROVIDER, UPDATE_FREQ,
+				MIN_DIST, gpsll, Looper.getMainLooper());
+		Log.i("GPSLocationLogger local", "Registered Location Listener.");
+	}
+
+	/**
+	 * This method removes the requested location updates.
+	 * 
+	 * @Override
+	 */
+	public void stopPlugin() {
+		if (!PLUGIN_ACTIVE) {
+			return;
+		}
+		gpsm.removeUpdates(gpsll);
+		Log.i("GPSLocationLogger local", "Unregistered Location Listener.");
 	}
 
 }
